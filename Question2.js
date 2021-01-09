6356867255427309b1b3429e00fcdc9100d2ab89
farazuddin1997 <72013848+farazuddin1997@users.noreply.github.com>
2021-01-09 20:54:33 +0530

    Add files via upload
    uploaded

--- Question2.js
+++ Question2.js
@@ -1,12 +1,11 @@
-// const arr = "010100111001111";
-
-// var count = 0;
-
-// var x;
-
-// for (x in arr) {
-//   if (x == 1 && arr[x] == arr[x + 1]) {
-//     count += 1;
-//   }
-// }
-// console.log(x);
+const arr = [0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0];
+var count = 0;
+var num = 0;
+var x;
+for (x of arr) {
+  if ((arr[x] == arr[x + 1] && arr[x] = 1)) {
+    count = count + 1;
+    num = count;
+  }
+}
+console.log(num);
